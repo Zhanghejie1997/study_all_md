f255795e304fb9fbf82ff98f656f9c6647ae6a33
zhanghejie <18805078159@163.com>
2023-09-14 16:14:50 +0800

    完善失败重复函数，并开始redis函数

--- javascript_demo/module/ms.js
+++ javascript_demo/module/ms.js
@@ -1,9 +1,5 @@
 /*
- * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
+ * Copyright (c) 2023.
  */
 
 //----------------------------------------------------------------------------------------
@@ -18,15 +14,13 @@ var log = {
 var async = {
     waterfall: (tasks, callback) => {
         var task_index = 0;
-
         function next() {
             var args = [].slice.call(arguments)
             if (task_index == tasks.length || args[0]) {
                 return callback.apply(this, args)
             }
 
-            args = args.slice(1)
-            args.push(next)
+            args = args.slice(1), args.push(next)
             try {
                 tasks[task_index++].apply(this, args)
             } catch (err) {
@@ -41,4 +35,4 @@ var async = {
 
 //----------------------------------------------------------------------------------------
 
-global.ms = {log, async, }
+global.ms = {log, async,}
--- javascript_demo/module/rerun_fn.js
+++ javascript_demo/module/rerun_fn.js
@@ -46,15 +46,16 @@ function rerun_fns(list, run_fn, callback, option) {
                 return setTimeout(one_fn, option.next_time)
             } else if (err_list.length && num < option.restart_number) {
                 run_list = err_list.map(i => i.index), err_list = [], run_index = 0, num++
-                console.log('num', num)
                 return setTimeout(one_fn, option.rerun_time)
+            } else {
+                return callback(err_list.length ? err_list : null, return_data)
             }
-            return callback(err_list.length ? err_list : null, return_data)
         }
         run_fn(i_item.item, cb, i_item.index)
     }
     one_fn()
 }
+
 //----------------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------------
@@ -74,19 +75,18 @@ function set_timeout_rerun_fns(list, run_fn, callback, option) {
     var num = 0, run_index = 0, err_list = [], run_list = list.map((i, index) => index)
     var return_index = 0
 
-    var time = setInterval(function (){
+    var time = setInterval(function () {
         var i_item = return_data[run_list[run_index]]
         var cb = (err, data) => {
             Object.assign(i_item, {err: err || null, data})
             err && err_list.push(i_item)
 
-            if(++return_index < run_list.length){
+            if (++return_index < run_list.length) {
                 return
             }
             if (err_list.length && num < option.restart_number) {
-                run_list = err_list.map(i => i.index), err_list = [], run_index = 0,return_index=0; num++
-                console.log('num', num)
-                return set_timeout_rerun_fns(list, )
+                run_list = err_list.map(i => i.index), err_list = [], run_index = 0, return_index = 0, num++
+                return set_timeout_rerun_fns(list,)
             } else {
                 return callback(err_list.length ? err_list : null, return_data)
             }
@@ -98,18 +98,58 @@ function set_timeout_rerun_fns(list, run_fn, callback, option) {
             return clearInterval(time)
         }
 
-    },option.next_time)
+    }, option.next_time)
+}
+
+//----------------------------------------------------------------------------------------
+/**
+ * 定时器加载list，完成后调用callback
+ * @param list
+ * @param run_fn
+ * @param callback
+ * @param option
+ */
+function start_load_ok(list, run_fn, callback, option) {
+    var run_list = list.map((item, index) => Object.assign({item, index}))
+    start_run(run_list, run_fn, callback, option, 0)
+}
+
+function start_run(run_list, run_fn, callback, option, num) {
+    var run_index = 0, cb_index = 0, max_length = run_list.length, err_list = [], return_data = []
+
+    var time = setInterval(() => {
+        var item = run_list[run_index]
+        var cb = (err, data) => {
+            Object.assign(item, {err: err || null, data})
+            err && err_list.push(item)
+            if (++cb_index >= max_length) {
+                if (err_list.length && num < option.restart_number) {
+                    return setTimeout(() => start_run(err_list, run_fn, (err, data) => {
+                        data && data.forEach(i => run_list[i.index] = i)
+                        callback(err, run_list)
+                    }, option, (num || 0) + 1), option.rerun_time)
+                }
+                return callback(err_list.length ? err_list : null, return_data)
+            }
+        }
+
+        run_fn(item.item, cb, item.index)
+
+        if (++run_index == max_length) {
+            return clearInterval(time)
+        }
+    }, option.next_time)
 }
 
 //----------------------------------------------------------------------------------------
 
-
+//----------------------------------------------------------------------------------------
 function test() {
     function test_rerun_fn() {
         var index = 0
         var fn = (cb, i) => {
             console.log('index', index, i)
-            cb(index++ < 60 ? 'err'+index : null, index)
+            cb(index++ < 60 ? 'err' + index : null, index)
         }
         rerun_fn(fn, 10, (err, data) => console.log(err, data))
     }
@@ -122,13 +162,36 @@ function test() {
         var fn = (item, cb, i) => {
             // console.log('test_rerun_fns index', item, i)
             index++
-            return cb( item < 20 ? 'err'+index  : null, i)
-            cb(index < 10 && item > 5 && item < 20 ? 'err'+index : null, i)
+            return cb(item < 20 ? 'err' + index : null, i)
+            cb(index < 10 && item > 5 && item < 20 ? 'err' + index : null, i)
         }
-        rerun_fns(test_list, fn, (err, data) => console.log(err, data),{restart_number: 10000,next_time:1,rerun_time:1})
+        rerun_fns(test_list, fn, (err, data) => console.log(err, data), {
+            restart_number: 10000,
+            next_time     : 1,
+            rerun_time    : 1
+        })
     }
 
     // test_rerun_fns()
+
+
+    function test_start_load_ok() {
+        var index = 0
+        var test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20]
+        var fn = (item, cb, i) => {
+            // console.log('test_rerun_fns index', item, i)
+            index++
+            // return cb(item < 20 ? 'err' + index : null, i)
+            cb(index < 10 && item > 5 && item < 20 ? 'err' + index : null, i)
+        }
+        start_load_ok(test_list, fn, (err, data) => console.log(err, data), {
+            restart_number: 3,
+            next_time     : 1,
+            rerun_time    : 5000
+        })
+    }
+
+    // test_start_load_ok()
 }
 
 test()
