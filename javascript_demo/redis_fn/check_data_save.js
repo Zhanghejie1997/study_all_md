b9fc7050624234330d8c96c6b38a65365656e6ba
zhanghejie <18805078159@163.com>
2023-09-19 16:35:45 +0800

    添加过滤函数

---
+++ javascript_demo/redis_fn/check_data_save.js
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2023.
+ */
+
+
+//----------------------------------------------------------------------------------------
+typeof ms === 'undefined' ? require('../module/test_ms') : null
+var info = ms.log.info
+var error = ms.log.error
+/**
+ * @config
+ * @type {{}}
+ */
+var config = {
+    line: [{
+        field  : 'field', check_fn: (val, cb, item) => {
+            var err = ''
+            cb(err, val)
+        }, type: '', save_data: '', save_fn: (val, save_data) => {
+        }, tip : '',
+    }],
+}
+
+//----------------------------------------------------------------------------------------
+var check_fn_all = {
+    is_null    : (val, cb, option) => cb([null, undefined, ''].indexOf(val) > -1 ? option.item.tip || (option.item.field + ' is is null') : null, val),
+    is_not_null: (val, cb, option) => cb([null, undefined, ''].indexOf(val) == -1 ? option.item.tip || (option.item.field + ' is is not null') : null, val),
+    page       : (val, cb, option) => {
+        val = val || {}
+        cb(null, {number: val.page || 20, index: val.index || 1})
+    },
+    def        : (val, cb, option) => cb(null, option.item.def || option.item.default),
+    default    : (val, cb, option) => cb(null, option.item.def || option.item.default),
+}
+
+//----------------------------------------------------------------------------------------
+function get_save_fn(save_str) {
+    var save_arr = (save_str || '').split('.')
+    var save_filed = save_arr.pop()
+    return (save_data, line, val) => {
+        save_arr.forEach(i => save_data = save_data[i] || (save_data[i] = {}))
+        save_data[save_filed || line] = val
+    }
+}
+
+function init_line(line) {
+    return line.map(i => {
+        i.check_fn = i.check_fn ||check_fn_all[i.type]
+        !i.check_fn || error('err not type:', i.type, line)
+        i.save_fn = i.save_fn || (i.save_data ? get_save_fn(i.save_data) : null)
+        return i
+    })
+}
+
+//----------------------------------------------------------------------------------------
+function check_data_init(config) {
+    var line = init_line(config.line)
+    var max_length = line.length
+    return (body, callback) => {
+        var index = 0, save_data = {}
+        var fn = () => {
+            var item = line[index] || {}
+            var field = item.field
+            item.check_fn(body[field], (err, save_val) => {
+                err || (item.save_fn ? item.save_fn(save_data, field, save_val, item) : save_data[field] = save_val)
+                err || ++index >= max_length ? callback(err, save_data) : fn()
+            }, {item, body})
+        }
+        fn()
+    }
+}
+
+//----------------------------------------------------------------------------------------
+function test() {
+    var fn = check_data_init({
+        line: [{
+            field  : 'field', check_fn: (val, cb, item) => {
+                var err = ''
+                cb(err, val)
+            }, type: '', save_data: '', save_fn: (save_data, field, val, item) => {
+                save_data[field] = val
+            }, tip : 'err',
+        }, {field: 'a', check_fn: (val, cb) => cb('', val)},
+            {field: 'b', check_fn: (val, cb) => cb('', val), save_data: 'b.b.c'},
+            {field: 'b', check_fn: (val, cb) => cb('', val), save_data: 'b.b.'},
+            {field: 'c', check_fn: (val, cb) => cb('', val), save_data: 'c.'},
+            {field: 'd', check_fn: (val, cb) => cb('', val), save_data: 'd'},
+            {field: 'e', check_fn: (val, cb) => cb('', val), save_data: '.'},
+            {field: 'page', type: 'page'},
+
+        ],
+    })
+    fn({field: 'field', a: 1, b: 2, c: 3, d: 4, e: 5, page: {index: 4}}, (err, check_data) => {
+        console.log('err', err, ',check:', check_data)
+    })
+}
+
+test()
+//----------------------------------------------------------------------------------------
--- javascript_demo/redis_fn/redis_init.js
+++ javascript_demo/redis_fn/redis_init.js
@@ -15,20 +15,106 @@ var def_config = {
     key_prefix: '',
     def_time  : 60 * 60 * 24 * 7,
 }
-
-//----------------------------------------------------------------------------------------
-
-//----------------------------------------------------------------------------------------
-function init(option) {
-    var _option = Object.assign({}, def_config, option)
-    return {
-        _option
-    }
+var def_redis_fn = (redis_name) => {
+    return () => ms.db.redis[redis_name]
 }
 
 //----------------------------------------------------------------------------------------
+function get_option(option) {
+    var _option = Object.assign({}, def_config, option)
+    var redis_name = _option.redis_name
+
+    if (redis_name) {
+        _option.redis_fn = _option.redis_fn || def_redis_fn(redis_name)
+        _option.info && (_option.info = _option.info.bind(this, '[' + redis_name + ']'))
+        _option.error && (_option.error = _option.error.bind(this, '[' + redis_name + ']'))
+    }
+
+    _option.get_key = (key) => _option.key_prefix + key
+    return _option
+}
+
+//----------------------------------------------------------------------------------------
+function init(option) {
+    var _option = get_option(option)
+    return {
+        _option,
+        get_key     : (key, cb) => {
+            var redis = _option.redis_fn(), key = _option.get_key(key)
+            _option.info(`get:key=${key}`)
+            get_key(redis, key, (err, data) => {
+                err && _option.error(`key=${key},err=${err}`)
+                cb(err, data)
+            })
+        },
+        set_key     : (key, val, cb) => this.set_key_time(key, val, _option.def_time, cb),
+        set_key_time: (key, val, key_time, cb) => {
+            var redis = _option.redis_fn(), key = _option.get_key(key), time = key_time || _option.def_time
+            var log = `set:key=${key},val=${val},time=${time}`
+            _option.info(log)
+            set_key(redis, key, val, time, (err, data) => {
+                err && _option.error(`${log},err=${err}`)
+                cb(err, data)
+            })
+        },
+        mget        : (keys, cb) => {
+            var redis = _option.redis_fn(), time = _option.def_time, arr = keys.map(key=> _option.get_key(key))
+            var log = `mget:key=${key}`
+            _option.info(log)
+            mget(redis, keys, (err, data)=>{
+                err && _option.error(`${log},err=${err}`)
+                cb(err, data)
+            })
+        },
+        mset        : (data, cb) => {
+            var redis = _option.redis_fn(), arr = [], time = _option.def_time
+            Object.keys(data).forEach(key => arr.push(_option.get_key(key), data[key]))
+
+            var log = `mset:arr=${arr.join(' ')},time=${time}`
+            _option.info(log)
+            mset(redis, data, time, (err, data) => {
+                err && _option.error(`${log},err=${err}`)
+                cb(err, data)
+            })
+        },
+        mset_time   : (data, time, cb) => mset(_option.redis_fn(), data, time, cb),
+        hmget       : (key, line, cb) => mget(),
+        hmset       : (key, data, cb) => mget(),
+        add         : (key, cb) => incr(_option.get_key(key), cb),
+        incr        : (key, cb) => incr(_option.get_key(key), cb),
+    }
+}
+
+
+//----------------------------------------------------------------------------------------
+function get_key(redis, key, callback) {
+    redis.get(key, callback)
+}
+
+
+//----------------------------------------------------------------------------------------
+function incr(redis, key ,callback){
+
+}
+//----------------------------------------------------------------------------------------
+function set_key(redis, key, value, time, callback) {
+
+}
+
+//----------------------------------------------------------------------------------------
+function mset(redis, data, time, callback) {
+    var data = Array.isArray(data) ? data : Object.keys(data).map()
+    redis.mset(arr)
+}
+
+function mget(redis, key, callback) {
+
+}
+
 
 //----------------------------------------------------------------------------------------
 module.exports = {
-    init: (option) => init(option)
+    init: (option) => init(option),
+    get_key,
+    set_key,
 }
